5843d56e9d8289843a0f724214558653ce1ae713
Tyler Szakacs <tylerszakacs@gmail.com>
2026-01-26 15:11:13 -0500

    fix: pass credits through layout.ts to template

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -26,5 +26,5 @@ export const load: LayoutLoad = async ({ data, depends, fetch }) => {
         data: { user }
     } = await supabase.auth.getUser();
 
-    return { session, supabase, user };
+    return { session, supabase, user, credits: data.credits };
 };
